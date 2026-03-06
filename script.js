889421fb68d262a31cdca48c8d20d5aed1ba3b67
manjarigoyal16 <manjarigoyal2616@gmail.com>
2026-03-06 22:21:23 +0530

    nothing

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-// let tablinks = document.getElementsByClassName('tab-links');
-// let tabcontents = document.getElementsByClassName('tab-contents');
-
-// // Function to handle switching between Skills, Experience, and Education tabs
-// function openTab(tabName) {
-//     for (let tablink of tablinks) {
-//         tablink.classList.remove("active-link");
-//     }
-//     for (let tabcontent of tabcontents) {
-//         tabcontent.classList.remove("active-tab");
-//     }
-//     // event is passed automatically by the browser
-//     event.currentTarget.classList.add("active-link");
-//     document.getElementById(tabName).classList.add("active-tab");
-// }
-
-// // Side Menu Logic for Mobile
-// let sidemenu = document.getElementById('sidemenu');
-
-// function openmenu() {
-//     sidemenu.style.right = "0";
-// }
-
-// function closemenu() {
-//     sidemenu.style.right = "-200px";
-// }
-
-// // Hamburger Toggle (Alternative logic for small screens)
-// document.getElementById('hamburger').addEventListener('click', function () {
-//     let navUl = document.querySelector('nav ul');
-//     // Simple toggle between showing and hiding the side menu
-//     if (sidemenu.style.right === "0px") {
-//         closemenu();
-//     } else {
-//         openmenu();
-//     }
-// });
 
 // Side Menu Logic
 let sidemenu = document.getElementById('sidemenu');
@@ -63,20 +26,6 @@ function toggleAccordion(header) {
     }
 }
 
-// Tab Switching Logic (For About Section)
-let tablinks = document.getElementsByClassName('tab-links');
-let tabcontents = document.getElementsByClassName('tab-contents');
-
-function openTab(tabName) {
-    for (let tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (let tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabName).classList.add("active-tab");
-}
 
 // Optional: Fix for hamburger button if you have one with ID 'hamburger'
 let hamburger = document.getElementById('hamburger');
